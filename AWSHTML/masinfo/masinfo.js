266bab90582012ac4be86d194d9a62bda28ee42b
Cova247 <covacar247@gmail.com>
2021-11-21 18:26:27 -0600

    más javascript

--- AWSHTML/masinfo/masinfo.js
+++ AWSHTML/masinfo/masinfo.js
@@ -5,29 +5,39 @@ let origen = urlParams.get('origen');
 let destino = urlParams.get('destino')
 let fecha = urlParams.get('fecha')
 let hora = urlParams.get('hora')
+let costo = urlParams.get('costo')
+let asientosdisp = urlParams.get('asientosdisp')
 
-console.log(origen, destino, fecha, hora);
+console.log(origen, destino, fecha, hora, costo, asientosdisp);
 
 let tablaOrigen = document.getElementById('origen')
 let tablaDestino = document.getElementById('destino')
 let tablaFecha = document.getElementById('fecha')
 let tablaHora = document.getElementById('hora')
+let tablaCosto = document.getElementById('costo')
+let tablaAsientosdisp = document.getElementById('asientosdisp')
 
 tablaOrigen.innerHTML = origen
 tablaDestino.innerHTML = destino
 tablaHora.innerHTML = hora
 tablaFecha.innerHTML = fecha
+tablaCosto.innerHTML = costo
+tablaAsientosdisp.innerHTML = asientosdisp
+
 
 console.log(valores);
 
+let contador = 0
 let ArregloBoletos = new Array;
 
 function agregarBoleto() {
     let boletos = document.getElementById('boletos-id')
     let fila = document.getElementById('fila-id').value, asiento = document.getElementById('asiento-id').value
+    contador = contador + 1
 
+if 
     let a = `
-    <tr>
+    <tr id="boleto${contador}">
     <td>
         Boleto
         <div class="input-group mb-3">
@@ -38,7 +48,7 @@ function agregarBoleto() {
         </div>
     </td>
     <td class="col-1">
-        <button type="button" class="btn btn-danger">Eliminar</button>
+        <button type="button" onclick="Borrarboleto(contador)" class="btn btn-danger">Borrar</button>
     </td>
     </tr>
     `
@@ -46,6 +56,11 @@ function agregarBoleto() {
 
     ArregloBoletos.push([fila, asiento])
 
+}
+
+function Borrarboleto(contador) {
+    let idboleto = document.getElementById("boleto"+contador)
+    idboleto.remove()
 }
 
 let nuevosValores = valores
